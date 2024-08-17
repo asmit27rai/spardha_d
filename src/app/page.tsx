19849a882241d6707299922d420544355bb1763b
asmit27rai <raiasmit10@gmail.com>
2024-08-18 05:05:16 +0530

    Some Basic Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
 "use client";
-import { motion } from "framer-motion";
 import React from "react";
+import { motion } from "framer-motion";
 import { AuroraBackground } from "@/components/ui/aurora-background";
 import { Heading } from "@/components/HeadingComponent";
 import { TypewriterEffectSmoothDemo } from "@/components/EventStarter";
 
+const Footer = () => {
+  return (
+    <footer className="text-white py-4 mt-8">
+      <div className="container mx-auto text-center">
+        <p className="text-lg font-semibold">Spardha Is Waiting For You</p>
+      </div>
+    </footer>
+  );
+};
+
 export default function Home() {
   return (
     <AuroraBackground>
-      <Heading/>
+      <Heading />
       <TypewriterEffectSmoothDemo />
+      <Footer />
     </AuroraBackground>
   );
 }
